790a2c3e012e0532e82b4b85d77231cbc5b47abc
Ben Denckla <bdenckla@alum.mit.edu>
2025-02-12 17:08:04 -0500

    precursor renaming

--- aleppo/main_make_wikisouce_page.py
+++ aleppo/main_make_wikisouce_page.py
@@ -6,13 +6,15 @@ from py.read_csv_file import read_csv_file
 
 def main():
     data_entries = read_csv_file(_CSV_IN_PATH, _JSON_OUT_PATH_1)
-    group_by_book(data_entries, _JSON_OUT_PATH_2)
+    grouped = group_by_book(data_entries, _JSON_OUT_PATH_2)
+    write_wikitext_file(grouped, _WIKITEXT_OUT_PATH)
     pass
 
 
 _CSV_IN_PATH = "aleppo/J David Stark Aleppo Codex Index.csv"
-_JSON_OUT_PATH_1 = "aleppo/J David Stark Aleppo Codex Index.json"
+_JSON_OUT_PATH_1 = "aleppo/index-flat.json"
 _JSON_OUT_PATH_2 = "aleppo/index-grouped-by-book.json"
+_WIKITEXT_OUT_PATH = "aleppo/index.wiki"
 
 
 if __name__ == "__main__":
