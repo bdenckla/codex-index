f88dff8e92e62c7e0ce70b7a2388856da9264db9
Ben Denckla <bdenckla@alum.mit.edu>
2025-02-12 18:23:48 -0500

    gaps

--- aleppo/py/write_wikitext_file.py
+++ aleppo/py/write_wikitext_file.py
@@ -1,7 +1,6 @@
 import py.my_open as my_open
 import py.hebrew_verse_numerals as hvn
 from py.book_names import LATIN_TO_HEBREW
-from py.my_utils import sl_map
 from py.my_utils import sum_of_map
 
 
@@ -15,7 +14,7 @@ def _lines_for_one_book(bkna_and_entries):
     heb_bkna = LATIN_TO_HEBREW[lat_bkna]
     line_1_for_book = "=== " + heb_bkna + " ==="
     line_2_for_book = "'''" + heb_bkna + " ...'''"
-    lines_for_entries = sl_map(_lines_for_one_entry, entries)
+    lines_for_entries = sum_of_map(_lines_for_one_entry, entries)
     return ["", line_1_for_book, line_2_for_book, *lines_for_entries]
 
 
@@ -23,12 +22,20 @@ def _lines_for_one_entry(entry):
     url = entry["de_url"]
     leaf = entry["de_leaf"]
     if not url:
-        return f'# {leaf} N/A'
+        return [f'# {leaf} N/A']
     visible = _heb_range(entry["de_text_range"])
     anchor = f"[{url} {visible}]"
     daf_num_ab = _rv_ab(leaf)
     daf = f"(דף {daf_num_ab})"
-    return "#" + anchor + " " + daf
+    main_line = "#" + anchor + " " + daf
+    gap = entry["de_gap"]
+    if not gap:
+        return [main_line]
+    if gap == "Before":
+        return ['# gap', main_line]
+    if gap == "After":
+        return [main_line, '# gap']
+    assert False, gap
 
 
 def _heb_range(text_range):
