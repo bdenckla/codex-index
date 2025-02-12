01ea17b65e46eb7eee80c99db2963439c0519eae
Ben Denckla <bdenckla@alum.mit.edu>
2025-02-12 17:20:11 -0500

    put basic wiki write structure in place

--- aleppo/py/write_wikitext_file.py
+++ aleppo/py/write_wikitext_file.py
@@ -1,2 +1,23 @@
+import py.my_open as my_open
+from py.my_utils import sl_map
+from py.my_utils import sum_of_map
+
 def write_wikitext_file(grouped, out_path):
-    return
+    lines = sum_of_map(_lines_for_one_book, grouped.items())
+    my_open.with_tmp_openw(out_path, {}, _write_callback, lines)
+
+
+def _lines_for_one_book(bkna_and_entries):
+    bkna, entries = bkna_and_entries
+    line_for_book = bkna
+    lines_for_entries = sl_map(_lines_for_one_entry, entries)
+    return [line_for_book, *lines_for_entries]
+
+
+def _lines_for_one_entry(entry):
+    return str(entry)
+
+
+def _write_callback(lines, out_fp):
+    for line in lines:
+        out_fp.write(line + "\n")
