41aaa87d677df50d9bfb1f876eebecdf10888ddc
Ben Denckla <bdenckla@alum.mit.edu>
2025-02-20 15:45:36 -0500

    first index.wiki output for LC

--- lenin-wiki/py/write_wikitext_file.py
+++ lenin-wiki/py/write_wikitext_file.py
@@ -1,7 +1,7 @@
 import py.my_open as my_open
 import py.hebrew_verse_numerals as hvn
-from py.book_names import LATIN_TO_HEBREW
-from py.my_utils import sum_of_map
+from py.mam_book_names_and_std_book_names import he_bk39_name
+from py.my_utils import sum_of_map, sl_map
 
 
 def write_wikitext_file(grouped, out_path):
@@ -10,44 +10,78 @@ def write_wikitext_file(grouped, out_path):
 
 
 def _lines_for_one_book(bkna_and_rows):
-    lat_bkna, rows = bkna_and_rows
-    heb_bkna = LATIN_TO_HEBREW[lat_bkna]
+    bk39id_mostly, rows = bkna_and_rows
+    if bk39id_mostly in (_MAS_LISTS_TORAH, _MAS_LISTS_PROPHETS):
+        heb_bkna = bk39id_mostly
+        lines_for_rows = sl_map((_line_for_non_bib_row, bk39id_mostly), rows)
+    else:
+        heb_bkna = he_bk39_name(bk39id_mostly)
+        lines_for_rows = sl_map(_line_for_bib_row, rows)
     line_1_for_book = "=== " + heb_bkna + " ==="
     line_2_for_book = "'''" + heb_bkna + " ...'''"
-    lines_for_rows = sum_of_map(_lines_for_one_row, rows)
     return ["", line_1_for_book, line_2_for_book, *lines_for_rows]
 
 
-def _lines_for_one_row(row):
-    url = row["de_url"]
-    leaf = row["de_leaf"]
-    if not url:
-        return [f'# {leaf} N/A']
-    visible = _heb_range(row["de_text_range"])
+def _line_for_bib_row(row):
+    page = row["page"]
+    assert page
+    url = f"https://manuscripts.sefaria.org/leningrad-color/BIB_LENCDX_F{page}.jpg"
+    visible = _heb_range(_text_range(row))
     anchor = f"[{url} {visible}]"
-    daf_num_ab = _rv_ab(leaf)
+    daf_num_ab = _cacb_hahb(page)
     daf = f"(דף {daf_num_ab})"
     main_line = "#" + anchor + " " + daf
-    gap = row["de_gap"]
-    if not gap:
-        return [main_line]
-    if gap == "Before":
-        return ['# gap', main_line]
-    if gap == "After":
-        return [main_line, '# gap']
-    assert False, gap
+    return main_line
+
+
+def _line_for_non_bib_row(visible, row):
+    page = row["page"]
+    assert page
+    url = f"https://manuscripts.sefaria.org/leningrad-color/BIB_LENCDX_F{page}.jpg"
+    anchor = f"[{url} {visible}]"
+    daf_num_ab = _cacb_hahb(page)
+    daf = f"(דף {daf_num_ab})"
+    main_line = "#" + anchor + " " + daf
+    return main_line
+
+
+def _text_range(row):
+    sta_bcvy = _mref(row, "startc", "startv", "startp", "startl")
+    sto_bcvy = _mref(row, "stopc", "stopv", "stopp", "stopl")
+    return sta_bcvy, sto_bcvy
+
+
+def _mref(row, keyc, keyv, keyp, keyl):
+    maxp = row[keyl]  # max value of p is the length of the verse
+    assert maxp != 1
+    if row[keyp] == 1:
+        fml = "fml-first"
+    elif row[keyp] == row[keyl]:
+        fml = "fml-last"
+    else:
+        fml = "fml-mid"
+    return row["bkid"], row[keyc], row[keyv], fml
 
 
 def _heb_range(text_range):
-    start_bcv, stop_bcv = text_range
-    sta = _heb_bcv(start_bcv)
-    sto = _heb_bcv(stop_bcv)
+    sta_bcvy, sto_bcvy = text_range
+    sta = _heb_bcv(sta_bcvy[:3])
+    sto = _heb_bcv(sto_bcvy[:3])
     sta_str = f'{sta[0]} {sta[1]},{sta[2]}'
-    abbr_stop_str = _abbreviated_stop(sta, sto)
-    return f"{sta_str}{_EN_DASH}{abbr_stop_str}"
+    abbr_sto_str = _abbreviated_sto(sta, sto)
+    bracs = _brackets(text_range)
+    return f"{bracs[0]}{sta_str}{_EN_DASH}{abbr_sto_str}{bracs[1]}"
 
 
-def _abbreviated_stop(sta, sto):
+def _brackets(text_range):
+    sta_bcvy, sto_bcvy = text_range
+    sta_fml, sto_fml = sta_bcvy[3], sto_bcvy[3]
+    sta_brac = "[" if sta_fml == "fml-first" else "("
+    sto_brac = "]" if sta_fml == "fml-last" else ")"
+    return sta_brac, sto_brac
+
+
+def _abbreviated_sto(sta, sto):
     if sta[0] == sto[0]:
         if sta[1] == sto[1]:
             return sto[2]
@@ -56,15 +90,17 @@ def _abbreviated_stop(sta, sto):
 
 
 def _heb_bcv(bcv):
-    lat_bkna, int_chnu, int_vrnu = bcv
-    heb_bkna = LATIN_TO_HEBREW[lat_bkna]
+    bk39id, int_chnu, int_vrnu = bcv
+    heb_bkna = he_bk39_name(bk39id)
     heb_chnu = hvn.INT_TO_STR_DIC[int_chnu]
     heb_vrnu = hvn.INT_TO_STR_DIC[int_vrnu]
     return heb_bkna, heb_chnu, heb_vrnu
 
 
-def _rv_ab(leaf):  # r becomes א; v becomes ב
-    return leaf[:-1] + _RV_AB[leaf[-1]]
+def _cacb_hahb(leaf):
+    # A (ca) (cap A) becomes א (ha) (Hebrew alef)
+    # B (cb) (cap B) becomes ב (hb) (Hebrew bet)
+    return leaf[:-1] + _CACB_HAHB[leaf[-1]]
 
 
 def _write_callback(lines, out_fp):
@@ -73,7 +109,6 @@ def _write_callback(lines, out_fp):
 
 
 _EN_DASH = "–"
-_RV_AB = {
-    'r': 'א',
-    'v': 'ב',
-}
+_CACB_HAHB = {'A': 'א', 'B': 'ב'}
+_MAS_LISTS_TORAH = "Masoretic lists: Number of verses and the sections in the Torah"
+_MAS_LISTS_PROPHETS = 'Masoretic lists: Number of verses and the sections in the Prophets'
