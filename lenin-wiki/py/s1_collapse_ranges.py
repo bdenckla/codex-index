37013a3ec112fe1f3951a8a4e07e565ff9d96e7f
Ben Denckla <bdenckla@alum.mit.edu>
2025-02-24 12:36:49 -0500

    get closer to real range-collapsing

--- lenin-wiki/py/s1_collapse_ranges.py
+++ lenin-wiki/py/s1_collapse_ranges.py
@@ -2,13 +2,23 @@ from py.my_utils import my_groupby
 
 
 def s1_collapse_ranges(rows):
-    return rows
-    grouped_by_page = my_groupby(rows, _get_page)
+    grouped_by_page = my_groupby(rows, _get_pb)
     return list(map(_collapse_ranges_of_one_page, grouped_by_page.values()))
 
 
+def _get_pb(row):
+    return _get_page(row), _get_book(row)
+
+
 def _get_page(row):
-    return row['page']
+    return row["page"]
+
+
+def _get_book(row):
+    # We treat the note on a non-Biblical row as a kind of pseudo-book.
+    key = row["bkid"] or row["note"]
+    assert key is not None
+    return key
 
 
 def _collapse_ranges_of_one_page(ranges_of1p):
--- lenin-wiki/py/s2_group_by_book.py
+++ lenin-wiki/py/s2_group_by_book.py
@@ -2,10 +2,11 @@ from py.my_utils import my_groupby
 
 
 def s2_group_by_book(rows):
-    return my_groupby(rows, _get_assigned_book)
+    return my_groupby(rows, _get_book)
 
 
-def _get_assigned_book(row):
-    key = row['bkid'] or row['note']
+def _get_book(row):
+    # We treat the note on a non-Biblical row as a kind of pseudo-book.
+    key = row["bkid"] or row["note"]
     assert key is not None
     return key
