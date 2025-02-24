9ed9a402b662d6c4c09a10dbbcc463de1246fda6
Ben Denckla <bdenckla@alum.mit.edu>
2025-02-24 12:38:06 -0500

    pb (page/book pair) not just page

--- lenin-wiki/py/s1_collapse_ranges.py
+++ lenin-wiki/py/s1_collapse_ranges.py
@@ -2,11 +2,12 @@ from py.my_utils import my_groupby
 
 
 def s1_collapse_ranges(rows):
-    grouped_by_page = my_groupby(rows, _get_pb)
-    return list(map(_collapse_ranges_of_one_page, grouped_by_page.values()))
+    grouped_by_pb = my_groupby(rows, _get_pb)
+    return list(map(_collapse_ranges_of_one_pb, grouped_by_pb.values()))
 
 
 def _get_pb(row):
+    # pb: page/book pair
     return _get_page(row), _get_book(row)
 
 
@@ -21,5 +22,6 @@ def _get_book(row):
     return key
 
 
-def _collapse_ranges_of_one_page(ranges_of1p):
-    return ranges_of1p[0]
+def _collapse_ranges_of_one_pb(ranges_of1pb):
+    # pb: page/book pair
+    return ranges_of1pb[0]
